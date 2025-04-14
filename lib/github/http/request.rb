03d8001d8ad5116e8a09d683a899da3c51507574
ykttdn <38461803+ykttdn@users.noreply.github.com>
2025-04-14 23:06:20 +0900

    Accept query params

--- lib/github/http/request.rb
+++ lib/github/http/request.rb
@@ -1,22 +1,33 @@
 module Github
   class Request
-    attr_reader :client, :resource
+    attr_reader :client, :resource, :opts
 
     BASE_URL = 'https://api.github.com'
 
-    def initialize(client, method, resource)
+    def initialize(client, method, resource, opts = {})
       @client = client
       @method = method
       @resource = resource
+      @opts = opts
     end
 
     def perform
-      url = URI.parse("#{BASE_URL}#{resource}")
-      http = Net::HTTP.new(url.host, url.port)
+      full_url = request_url
+      uri = URI.parse(full_url)
+      http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       headers = { 'Authorization' => "token #{client.token}" }
-      response = http.get(url.path, headers)
+      response = http.get(full_url, headers)
       Response.new(response)
+    end
+
+    private
+
+    def request_url
+      url = "#{BASE_URL}#{resource}"
+      query = opts.map { |k, v| "#{k}=#{v}" }.join('&')
+      url += "?#{query}" unless opts.empty?
+      url
     end
   end
 end
