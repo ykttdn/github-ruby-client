bd065f9039fc7c53847355a159f943f67c96e7bd
ykttdn <38461803+ykttdn@users.noreply.github.com>
2025-04-17 01:59:11 +0900

    Add reader for repository owner

--- lib/github/repository.rb
+++ lib/github/repository.rb
@@ -2,11 +2,12 @@
 
 module Github
   class Repository
-    attr_reader :id, :name, :private
+    attr_reader :id, :name, :owner, :private
 
-    def initialize(id, name, private)
+    def initialize(id, name, owner, private)
       @id = id
       @name = name
+      @owner = owner
       @private = private
     end
 
@@ -14,7 +15,7 @@ module Github
       def all(user, opts = {})
         res = Request.new(user.client, :get, '/user/repos', opts).perform
         res.body.map do |repo|
-          new(repo[:id], repo[:name], repo[:private])
+          new(repo[:id], repo[:name], user, repo[:private])
         end
       end
     end
